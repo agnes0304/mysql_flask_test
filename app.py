13da9a8dc9ace923d100037a9539be3296dc9532
agnes0304 <jiwoochoi0304@gmail.com>
2022-03-30 16:06:25 +0900

    ongoing

--- app.py
+++ app.py
@@ -1,21 +1,71 @@
 import mysql.connector
 
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/")
-# def hello():
-#     return "<p>Hello!</p>"
+@app.route("/")
+def WELCOME():
+    return "<p>WELCOME!</p>"
+
 
 dataBase = mysql.connector.connect(
   host ="localhost",
   user ="root",
   passwd ="gg04236@@",
-  database ="student"
+  database ="SNU"
 )
 
 cursor = dataBase.cursor()
+
+
+
+# CREATE
+@app.route("/student", methods=["POST"])
+def add_student():
+  pass
+
+
+
+# READ
+# 전체 학생 조회
+@app.route("/student", methods=["GET"])
+def student():
+  pass
+
+# 학생 이름으로 조회
+@app.route("/student/<name>", methods=["GET"])
+def get_student_by_name(name):
+  pass
+
+
+
+# UPDATE
+# 이름, pw입력 (pw는 json 형태로 받기)
+@app.route("/student/<name>", methods=["PUT"])
+def update_student_by_name(name):
+  pass
+
+# code 알고 있다고 가정
+@app.route("/student/<code>", methods=["PUT"])
+def update_student_by_code(code):
+  pass
+
+
+
+# DELETE
+# 이름, pw입력 (pw는 json형태로 받기)
+@app.route("/student/<name>", methods=["DELETE"])
+def delete_student_by_name(name):
+  pass
+
+# code 알고 있다고 가정
+@app.route("/student/<code>", methods=["DELETE"])
+def delete_student_by_code(code):
+  pass
+
+
+
 
 query = ("SELECT * FROM student WHERE name = 'B'")
 cursor.execute(query)
@@ -23,6 +73,6 @@ cursor.execute(query)
 for (name) in cursor:
     print("{}".format(name))
 
-
 cursor.close()
+
 dataBase.close()
